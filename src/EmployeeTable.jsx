5485e2048be6542670757abd7ec8d5c0999a65bb
ashik <mohammedaashik4398@gmail.com>
2024-09-18 14:06:35 +0530

    XPagination

--- src/EmployeeTable.jsx
+++ src/EmployeeTable.jsx
@@ -50,9 +50,15 @@ function EmployeeTable() {
 
     return (
         <div style={{ padding: '20px' }}>
-            <h1 style={{ display: 'flex', justifyContent: 'center' }}>Employee Data Table</h1>
+            <h1 style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>Employee Data Table</h1>
             <table border="1" cellPadding="10" cellSpacing="0" style={{ width: '100%', textAlign: 'left' }}>
-                <thead style={{ backgroundColor: '#25be7d', color: 'white' }}>
+                <thead style={{
+                    backgroundColor: '#25be7d',
+                    color: 'white'
+                }}>
                     <tr>
                         <th>ID</th>
                         <th>Name</th>
@@ -73,16 +79,17 @@ function EmployeeTable() {
             </table>
 
             {/* Pagination Controls */}
-            <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '10px' }}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                paddingTop: '5px'
+            }}>
                 <button
                     onClick={handlePrevious}
                     disabled={currentPage === 1}
                     style={{
                         backgroundColor: '#25be7d',
                         color: 'white',
-                        border: 'none',
-                        padding: '10px 20px',
-                        cursor: currentPage === 1 ? 'not-allowed' : 'pointer',
                         margin: '0 5px'
                     }}
                 >
@@ -92,8 +99,6 @@ function EmployeeTable() {
                     style={{
                         backgroundColor: '#25be7d',
                         color: 'white',
-                        border: 'none',
-                        padding: '10px 20px',
                         margin: '0 5px'
                     }}
                 >
@@ -105,9 +110,6 @@ function EmployeeTable() {
                     style={{
                         backgroundColor: '#25be7d',
                         color: 'white',
-                        border: 'none',
-                        padding: '10px 20px',
-                        cursor: currentPage === totalPages ? 'not-allowed' : 'pointer',
                         margin: '0 5px'
                     }}
                 >
